063e1775c968bd312d826b2b94dc066e5cdf8dc6
Barry Tikalsky <tikal004@umn.edu>
2024-01-10 09:28:16 -0600

    disk audit

---
+++ disk_audit.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import argparse
+import pandas as pd
+import os
+from datetime import datetime, timedelta, date
+
+def format_bytes(size):
+    # https://stackoverflow.com/a/49361727
+    # 2**10 = 1024
+    power = 2**10
+    n = 0
+    power_labels = {0 : 'B', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
+    while size > power:
+        size /= power
+        n += 1
+    return f"{round(size, 1)}{power_labels[n]}"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", help="The directory to audit")
+parser.add_argument("--min-days", "--days", help="Only show files older than '--days' days old. Default 0 days.", default=0, type=int)
+parser.add_argument("--min-size", "--size", help="Only show files larger than this many bytes. Default 0 bytes.", default=0, type=int)
+args = parser.parse_args()
+
+base_path = os.path.abspath(args.directory)
+now = datetime.now()
+date_limit = datetime.timestamp(now - timedelta(days=args.min_days))
+
+data = []
+
+for root, dirs, files in os.walk(base_path):
+    for file in files:
+        filepath = root + '/' + file
+        try:
+            size = os.path.getsize(filepath)
+            creation_time = os.path.getctime(filepath)
+            modification_time = os.path.getmtime(filepath)
+            access_time = os.path.getatime(filepath)
+            data.append([size, filepath, creation_time, modification_time, access_time])
+        except Exception as err:
+            print(err)
+
+df = pd.DataFrame(data, columns=['size','filepath','created','modified','accessed'])
+df = df.sort_values(by='size', ascending=False)
+df = df.loc[df['created'] <= date_limit]
+df = df.loc[df['size'] >= args.min_size]
+df['created'] = df['created'].apply(lambda x: date.fromtimestamp(x))
+df['modified'] = df['modified'].apply(lambda x: date.fromtimestamp(x))
+df['accessed'] = df['accessed'].apply(lambda x: date.fromtimestamp(x))
+df['size'] = df['size'].apply(lambda x: format_bytes(x))
+
+df.to_csv('disk_audit.csv', index=False)
